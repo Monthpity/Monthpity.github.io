7da0dfbdfec6847b720dfbc5570dba7e145b9e04
Monthpity <1171947030@qq.com>
2024-01-16 11:34:23 +0800

    Site updated: 2024-01-16 11:34:17

--- js/matery.js
+++ js/matery.js
@@ -106,6 +106,11 @@ $(function () {
             subHtmlSelectorRelative: true
         });
 
+        // 懒加载防止插件冲突
+        $(document).find('img[data-original]').each(function(){
+            $(this).parent().attr("href", $(this).attr("data-original"));
+            });
+            
         // progress bar init
         const progressElement = window.document.querySelector('.progress-bar');
         if (progressElement) {
